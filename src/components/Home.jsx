9c7eb2ee191c1d1300b3d9e420e4e8024e19b38d
Blackxok <nasridoni1@gmail.com>
2024-10-09 23:15:28 +0500

    add Api 2

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,22 +4,23 @@ import OutputCom from './OutputCom'
 
 const Home = () => {
 	const [loader, setLoader] = useState(false)
+	const [result, setResult] = useState(null)
 
 	return (
 		<>
 			<div className='container home'>
 				<div className='column'>
-					<InputCom setLoader={setLoader} />
+					<InputCom setLoader={setLoader} setResult={setResult} />
 				</div>
 				{loader && (
 					<div className='column loader_col'>
 						<div className='loader_container'>
-							<div class='spinner-border loader' role='status'></div>
+							<div className='spinner-border loader' role='status'></div>
 						</div>
 					</div>
 				)}
 				<div className='column'>
-					<OutputCom />
+					<OutputCom result={result} />
 				</div>
 			</div>
 		</>
